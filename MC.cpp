596b545cd67dd7e1bf38291bb22a44729320f34a
steins <morgane.steins@ensta.fr>
2019-10-07 11:08:59 +0200

    tentative source uniforme : solution et MC faux

--- MC.cpp
+++ MC.cpp
@@ -5,9 +5,9 @@
 #include <cstdlib>
 
 using namespace std;
-
-//tirage de départ de neutrons arrivés en 1
-vector_points no_scattering_MC(int N, double mu){
+/* CAS HOMOGENE SOURCE PONCTUELLE */
+//tirage de départ de neutrons dans un matériau homogène, source ponctuelle en 0.
+vector_points no_scattering_homog_point_MC(int N, double mu){
     vector_points selection(N);
     for (int i=0; i<N; i++){
         selection.points[i] = point(parcourt_x(point(0,mu)),mu);
@@ -16,10 +16,10 @@ vector_points no_scattering_MC(int N, double mu){
 }
 
 // calcul de la densité neutronique au point x direction mu avec N tirages
-// avec une source ponctuelle isotrope en 0
-double density_no_scattering_MC(point p, int N){
+// avec une source ponctuelle isotrope en 0 et matériau homogène.
+double density_no_scattering_homog_point_MC(point p, int N){
     vector_points selection(N) ;
-    selection = no_scattering_MC(N,p.get_mu());
+    selection = no_scattering_homog_point_MC(N,p.get_mu());
     double rsl = 0.;
     for (int i=0;i<N;i++){
         if (selection.points[i].get_x()>p.get_x()) {
@@ -30,7 +30,42 @@ double density_no_scattering_MC(point p, int N){
     return rsl/(N*p.get_mu());
 }
 
-//calcul de la solution analytique sans absorption
-double density_no_scattering(point p){
+//calcul de la solution analytique sans scattering, cas homogène 
+//et source ponctuelle
+double density_no_scattering_homog_point(point p){
     if (p.get_mu()==0) {if (p.get_x()==0){return 1;} return 0;}
     return (1/p.get_mu())*exp(-p.sigmaT*p.get_x()/p.get_mu());}
+
+
+
+/* CAS HOMOGENE SOURCE UNIFORME */ 
+//tirage de départ de neutrons dans un matériau homogène, source uniforme 
+//arrivés en (x,mu).
+vector_points no_scattering_homog_unif_MC(int N, point p){
+    vector_points selection(N);
+    for (int i=0; i<N; i++){
+        selection.points[i] = deplacement_x(point(p.get_x(), -p.get_mu()));
+    }
+    return selection;
+}
+
+// calcul de la densité neutronique au point x direction mu avec N tirages
+// avec une source isotrope uniforme et matériau homogène.
+double density_no_scattering_homog_unif_MC(point p, int N){
+    vector_points selection(N) ;
+    selection = no_scattering_homog_unif_MC(N,p);
+    double rsl = 0.;
+    for (int i=0;i<N;i++){
+        if (selection.points[i].get_x()>0 && selection.points[i].get_x()<1) {
+            rsl++;
+        }
+    }
+    //print_vector(selection);
+    return rsl/(N*abs(p.get_mu()));
+}
+
+//calcul de la solution analytique sans scattering, cas homogène 
+//et source uniforme : disjonction cas sur signe de mu 
+double density_no_scattering_homog_unif(point p){
+    if (p.get_mu()>0) {return 1-exp(-p.sigmaT*p.get_x()/p.get_mu());}
+    return 1-exp(p.sigmaT*(1-p.get_x())/p.get_mu());}
--- main.cpp
+++ main.cpp
@@ -12,15 +12,18 @@ using namespace std;
 int main() {
     srand (static_cast <unsigned> (time(0)));
     
-    vector_points sol;
-    sol = no_scattering_MC(2,0.);
     //print_vector(sol);
-    double x = 1;
-    double mu = 0.25;
+    double x = 0.5;
+    double mu = 0.5;
     double integrale = 0.;
     cout<<"On travaille à x="<<x<<" et mu="<<mu<<endl;
-
-
+    double density = density_no_scattering_homog_unif_MC(point(x,mu), 100000);
+    cout<<"densité par MC "<<density<<endl;
+    cout<<"densité réelle "<<density_no_scattering_homog_unif(point(x,mu))<<endl<<endl;
+    
+    for (int i=0;i<1000;i++) {integrale += (15./1000.)*density_no_scattering_homog_unif(point((float) i*15/1000,mu));}
+    cout<<integrale<<endl;
+    //for (int i=0;i<11;i++) {cout<<(float)i/10<<" vaut "<<density_no_scattering_homog_unif_MC(point(float(i)/10,mu),10000)<<endl;}
     /* test du flux non collisionné : intégrale somme à 1*/
     /*double density = density_no_scattering_MC(point(x,mu), 100000);
     cout<<"densité par MC "<<density<<endl;

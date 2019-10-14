38810951dcd28c690c88a846d5c47b05440c6573
steins <morgane.steins@ensta.fr>
2019-10-14 22:58:17 +0200

    fin de rapport partie morgane, Enjoy

--- main.cpp
+++ main.cpp
@@ -18,39 +18,39 @@ int main() {
     double mu = 1;
     point p(x,mu);
 
-    // QUESTION 4
-    // Calcul de la densité au point (x,mu)
+    // // QUESTION 4
+    // // Calcul de la densité au point (x,mu)
     int N = 1e5;
-    cout<<"Calcul de phi q4 au point ("<<x<<","<<mu<<") avec "<<N<<" tirages"<<endl;
-    cout<<"densité par MC "<<density_no_scattering_homog_point_MC(point(x,mu), N)<<endl;
-    cout<<"densité réelle "<<density_no_scattering_homog_point(point(x,mu))<<endl;
+    // cout<<"Calcul de phi q4 au point ("<<x<<","<<mu<<") avec "<<N<<" tirages"<<endl;
+    // cout<<"densité par MC "<<density_no_scattering_homog_point_MC(point(x,mu), N)<<endl;
+    // cout<<"densité réelle "<<density_no_scattering_homog_point(point(x,mu))<<endl;
     
-    //calcul de la densité en nb_points intervalles et sauvée dans un fichier txt pour les plots
+    // //calcul de la densité en nb_points intervalles et sauvée dans un fichier txt pour les plots
     int nb_points = 25;
-    N = 1e4;
-    cout<<"N = "<<N<<" et on étudie "<<nb_points<<" intervalles."<<endl<<endl;
-    density_segment_no_scattering_homog_point_MC(N,nb_points,1);
+    // N = 1e4;
+    // cout<<"N = "<<N<<" et on étudie "<<nb_points<<" intervalles."<<endl<<endl;
+    // density_segment_no_scattering_homog_point_MC(N,nb_points,1);
 
 
-    // QUESTION 5
-    // Calcul de la densité au point (x,mu)
-    N = 1e6;
-    cout<<"Calcul de phi q5 au point ("<<x<<","<<mu<<") avec "<<N<<" tirages"<<endl;
-    cout<<"densité par MC "<<density_no_scattering_homog_unif_MC(point(x,mu), N)<<endl;
-    cout<<"densité réelle "<<density_no_scattering_homog_unif(point(x,mu))<<endl;
+    // // QUESTION 5
+    // // Calcul de la densité au point (x,mu)
+    // N = 1e6;
+    // cout<<"Calcul de phi q5 au point ("<<x<<","<<mu<<") avec "<<N<<" tirages"<<endl;
+    // cout<<"densité par MC "<<density_no_scattering_homog_unif_MC(point(x,mu), N)<<endl;
+    // cout<<"densité réelle "<<density_no_scattering_homog_unif(point(x,mu))<<endl;
 
-    //calcul de la densité en nb_points intervalles et sauvée dans un fichier txt pour les plots
-    nb_points = 25;
-    N = 1e6;
-    cout<<"N = "<<N<<" et on étudie "<<nb_points<<" intervalles."<<endl<<endl;
-    density_segment_no_scattering_homog_unif_MC(N,nb_points,p);
+    // //calcul de la densité en nb_points intervalles et sauvée dans un fichier txt pour les plots
+    // nb_points = 25;
+    // N = 1e5;
+    // cout<<"N = "<<N<<" et on étudie "<<nb_points<<" intervalles."<<endl<<endl;
+    // density_segment_no_scattering_homog_unif_MC(N,nb_points,p);
     
 
 
     // QUESTION 8
     //Cette fois ci on ne calcule que le flux moyenné en mu
     nb_points = 25;
-    N = 1e6;
+    N = 1e5;
     int max_iter = 1e5;
     double epsilon = 1e-10;
 
